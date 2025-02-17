08bf0d83ef573cc4dba00278196cea107b3e7dbe
Derek Bal <bal.derek.j@gmail.com>
2025-02-17 11:08:15 -1000

    another go

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://balderekjk2.github.io",
+  base: "https://balderekjk2.github.io",
 });
