80491f072e28dcfcc24758b9e44a2936fbf35943
Derek Bal <bal.derek.j@gmail.com>
2025-02-17 11:10:40 -1000

    dont pass in opts for site or base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://balderekjk2.github.io",
-  base: "https://balderekjk2.github.io",
-});
+export default defineConfig({});
