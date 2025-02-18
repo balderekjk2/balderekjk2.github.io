f7a2f4cee797dfe559a439237dddf28723beddb8
Derek Bal <bal.derek.j@gmail.com>
2025-02-17 19:43:41 -1000

    carry over layout of old portfolio, port to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://balderekjk2.github.io",
+  integrations: [mdx(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
